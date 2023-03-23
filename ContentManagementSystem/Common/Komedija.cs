d0b087248cbb8a8d88f6cf2d6b9c1c9c1fa03a0f
Marko Ankić <120253601+Z3ka1@users.noreply.github.com>
2023-03-23 21:21:16 +0100

    Dizajn strane za prijavu
    Dizajnirana strana za prijavu, napravljen "kostur" projekta.

---
+++ ContentManagementSystem/Common/Komedija.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Komedija
+    {
+        private string ime;
+        private int trajanje;
+        private string opis;
+        private string slika;
+        private string rtfRef;
+
+        public string Ime { get => ime; set => ime = value; }
+        public int Trajanje { get => trajanje; set => trajanje = value; }
+        public string Slika { get => slika; set => slika = value; }
+        public string Opis { get => opis; set => opis = value; }
+
+        public Komedija()
+        {
+
+        }
+
+        public Komedija(string ime, int trajanje, string opis, string slika, string rtfRef)
+        {
+            this.ime = ime;
+            this.trajanje = trajanje;
+            this.opis = opis;
+            this.slika = slika;
+            this.rtfRef = rtfRef;
+        }
+
+
+    }
+}
